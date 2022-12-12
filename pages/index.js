cde57792d07d6f0368045a2904460cd053cda05d
Jaysheel Dodia <73154873+jaysheel-ops@users.noreply.github.com>
2022-12-12 16:26:47 +0530

    github commit

--- pages/index.js
+++ pages/index.js
@@ -13,57 +13,43 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="#">Jay.js!</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Get started by contacting me @{' '}
+          <code className={styles.code}>jgdodia09@gmail.com</code>
+          <br />
+          <br />
+          Or get to know more about me @{' '}
+          <code className={styles.code}><a href="https://jaysheeldodia.web.app">jaysheeldodia.web.app</a></code>
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
+        <p>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://github.com"
           target="_blank"
           rel="noopener noreferrer"
+          style={{margin: "auto",}}
         >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
+          Powered by
+         
         </a>
-      </footer>
+        
+        </p>
+
+        <span className={styles.logo}>
+            <Image 
+              src="/githublogo.png" 
+              alt="Vercel Logo" 
+              width={120} 
+              height={68} 
+              style={{margin: "auto"}} 
+            />
+          </span>
+
+        
+      </main>
     </div>
   )
 }
