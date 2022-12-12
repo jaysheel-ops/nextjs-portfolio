af4a18ddad32b7cd2b04ca742fa3df2b4f4b0333
Jaysheel Dodia <73154873+jaysheel-ops@users.noreply.github.com>
2022-12-12 16:39:40 +0530

    image update

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-
+import githublogo from "/public/githublogo.png"
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -39,7 +39,7 @@ export default function Home() {
 
         <span className={styles.logo}>
             <Image 
-              src="/githublogo.png" 
+              src={githublogo}
               alt="Github Logo" 
               width={120} 
               height={68} 
